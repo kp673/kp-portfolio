1aa10f09e8666adb7cc467a9a5cd122df8cbe4dc
Kush Patel <55899517+kp673@users.noreply.github.com>
2021-09-25 21:12:22 -0400

    added experience

--- script.js
+++ script.js
@@ -19,4 +19,25 @@ $(document).ready(function() {
         backSpeed: 60,
         loop: true
     });
+    //owl carousel
+    $(".carousel").owlCarousel({
+        margin: 20,
+        loop: false,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
